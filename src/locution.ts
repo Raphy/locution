d3c4d52608a6a937bde4bb093dfae157a5b55df6
Raphael De Freitas <raphael@de-freitas.net>
2018-08-30 10:41:18 +0200

    Clean the function evaluate

--- src/locution.ts
+++ src/locution.ts
@@ -1,29 +1,15 @@
 import { Lexer } from './lexer';
-import { Node } from './node';
 import { Parser } from './parser';
 
 /**
  *
  */
 export class Locution {
-    /**
-     * Evaluate an expression with a given identifiers
-     *
-     * @param expression
-     * @param identifiers
-     */
+    private _lexer: Lexer = new Lexer();
+
+    private _parser: Parser = new Parser();
+
     public evaluate(expression: string, identifiers: object = {}): any {
-        // console.log('Locution Evaluate', expression, identifiers);
-
-        const lexer = new Lexer();
-        const tokenStream = lexer.tokenize(expression);
-        // console.log('TokenStream', tokenStream, tokenStream.token);
-
-        const parser = new Parser();
-        const rootNode: Node = parser.parse(tokenStream, identifiers);
-
-        // console.log('RootNode', rootNode, JSON.stringify(rootNode));
-
-        return rootNode.evaluate(identifiers);
+        return this._parser.parse(this._lexer.tokenize(expression), identifiers).evaluate(identifiers);
     }
 }
